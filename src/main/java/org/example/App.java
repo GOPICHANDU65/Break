a22a931e0a8a278eef259c448caed14c4de05e9f
appukappu <aparnareddy312@gmail.com>
2024-01-02 16:26:20 +0530

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class App 
+{
+    public void app(){
+        System.out.println("validt app");
+    }
+}
---
+++ src/main/java/org/example/Break.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Break {
+    public void bb(){
+        System.out.println("call break");
+    }
+}
